fda4e9b40c718fb8db25f48c5cba60aac2a8df9c
thanaporn029 <s64122250029@ssru.ac.th>
2025-04-05 03:18:32 +0700

    first commit

--- src/hi.jsx
+++ src/hi.jsx
@@ -1,25 +1,8 @@
-import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "./hi.css";
 
 function HI() {
-  const noBtnRef = useRef(null);
   const navigate = useNavigate(); // ✅ สำหรับเปลี่ยนหน้า
-
-  const moveNoButton = () => {
-    const button = noBtnRef.current;
-    const card = button.parentElement;
-
-    const maxX = card.clientWidth - button.clientWidth;
-    const maxY = card.clientHeight - button.clientHeight;
-
-    const randomX = Math.floor(Math.random() * maxX);
-    const randomY = Math.floor(Math.random() * maxY);
-
-    button.style.position = "absolute";
-    button.style.left = `${randomX}px`;
-    button.style.top = `${randomY}px`;
-  };
 
   const handleYesClick = () => {
     navigate("/hi"); // ✅ กลับไปหน้า App.jsx
@@ -31,9 +14,6 @@ function HI() {
       <h2>Ready?</h2>
       <div className="card">
         <button onClick={handleYesClick}>Yes</button>
-        <button id="no-button" ref={noBtnRef} onMouseEnter={moveNoButton}>
-          No
-        </button>
       </div>
     </>
   );
